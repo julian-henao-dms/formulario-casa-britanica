aaa0737e07f9a8117452faade9ec6be7e3d3e98e
Julian Henao DMS <julian.henao@dms.ms>
2022-05-06 17:18:44 -0500

    Fase de estructuración del formulario de inventario y los datos.
    Edita datos de interface InventarioVehiculosUsados para tipar el objeto que se
    enviará a través del onSubmit del formulario de inventario.
    
    Importa componentes y modulos de material para construir los elementos
    del formulario.
    
    Estructuración inicial de elementos de material para diseñar el formulario según los
    campos del formulario pdf de referencia.
    
    Añade reglas css globales y en componentes para ajustes iniciales de
    distribución de los campos del formulario.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HomeComponent } from './components/home/home.component';
 import { InventarioVehiculosUsadosComponent } from './components/inventario-vehiculos-usados/inventario-vehiculos-usados.component';
 import { HeaderInventarioComponent } from './components/header-inventario/header-inventario.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -23,6 +26,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -52,6 +56,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTooltipModule,
     FormsModule,
     ReactiveFormsModule,
+    MatExpansionModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/inventario-vehiculos-usados/inventario-vehiculos-usados.component.ts
+++ src/app/components/inventario-vehiculos-usados/inventario-vehiculos-usados.component.ts
@@ -8,14 +8,85 @@ import { InventarioVehiculosUsados } from './interfaces/inventario-vehiculos-usa
 })
 export class InventarioVehiculosUsadosComponent implements OnInit {
 
-  SI = 1;
-  NO = 0;
+  gasolina = 1;
+  diesel = 0;
+  automatica = 1;
+  mecanica = 0;
+  t4x2 = 0;
+  t4x4 = 1;
+  tela = 0;
+  cuero = 1;
+  nb = 0;
+  hb = 1;
+  qp = 2;
+
   public data: InventarioVehiculosUsados;
 
   constructor() { 
     this.data = {
+      consignacion: '',
+      retoma: '',
+      otros: '',
+      placa: '',
+      interno: '',
+      asesor: '',
+      sala: '',
       fecha: new Date,
       hora: new Date,
+      marca: '',
+      referencia: '',
+      modelo: '',
+      combustible: 0,
+      kilometraje: 0,
+      transmision: 0,
+      color: '',
+      cilindraje: 0 ,
+      traccion: '',
+      cojineria: '',
+      linea: '',
+      codigo_estado: '',
+      vida_llanta_del_izq: 0,
+      vida_llanta_del_der: 0,
+      vida_llanta_tras_izq: 0,
+      vida_llanta_tras_der: 0,
+      marca_llanta_del_izq: '',
+      marca_llanta_del_der: '',
+      marca_llanta_tras_izq: '',
+      marca_llanta_tras_der: '',
+      observacion_llanta: '',
+      rin: '' ,
+      tapa: '',
+      marca_rines_tapa: '',
+      observacion_rines: '',
+      matricula: 0,
+      soat: 0,
+      rtm: 0,
+      observaciones_documentos: '',
+      llaves:  0,
+      repuesto_llaves:  0,
+      manual: 0,
+      encendedor: 0,
+      cruceta: 0,
+      item_llave: 0,
+      trinquete_reversible: 0,
+      gato: 0,
+      kit_herramientas: 0,
+      antena: 0,
+      cubre_maletero: 0,
+      nivel_gasolina: 0,
+      marca_bateria: '',
+      ref_bateria: '',
+      marca_radio: '',
+      ref_radio: '',
+      observaciones: '',
+      nombre_entrega_vehiculo: '',
+      fecha_hora_entrega: new Date,
+      firma_entrega: '',
+      empleado_recibe: '',
+      nombre_recoge_vehiculo: '',
+      fecha_hora_recogida: new Date,
+      firma_recoge: '',
+      empleado_entrega: '',
     }
   }
 
